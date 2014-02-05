ae1b8f186cb17861fa8027818674866051e7494e
Mark Ma <gbstack08@gmail.com>
2014-02-05 17:13:28 +0800

    add Log console window

--- main.py
+++ main.py
@@ -30,8 +30,11 @@ class MainWindow(QWidget):
 		password_label = QLabel('Password')
 		self.password_edit = QLineEdit()
 		upload_btn = QPushButton('Upload')
-
 		upload_btn.clicked.connect(self.uploadBtnClicked)
+
+		self.console_box = QTextEdit()
+		self.console_box.setFixedHeight(100)
+		grid_layout.addWidget(self.console_box, 6, 0, 1, 3)
 
 		grid_layout.addWidget(address_label, 0, 1, 1, 1)
 		grid_layout.addWidget(self.address_edit, 0, 2, 1, 1)
@@ -41,14 +44,25 @@ class MainWindow(QWidget):
 		grid_layout.addWidget(self.password_edit, 2, 2, 1, 1)
 		grid_layout.addWidget(upload_btn, 3, 1, 1, 1)
 
+	def printTextToConsole(self, text):
+		print text
+		original_text = self.console_box.toPlainText()
+		self.console_box.setText(original_text+text+'\n')
+
 	def uploadBtnClicked(self):
 		server_address = self.address_edit.text()
 		username = self.username_edit.text()
 		password = self.password_edit.text()
 
-		ftp = ftplib.FTP(server_address)
-		print ftp
-		print ftp.login(username, password)
+		ftp = ftplib.FTP()
+		try:
+			print ftp.connect(server_address)
+		except Exception, e:
+			print e
+			self.printTextToConsole('Cannot connect to host')
+			return
+
+		self.printTextToConsole(ftp.login(username, password))
 
 		print username, password
 
